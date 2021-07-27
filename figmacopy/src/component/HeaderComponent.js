2e215d0a185cdb950b8899199097c4bb311e7523
abhishekshah <56739982+abhishekshah0036@users.noreply.github.com>
2021-07-27 21:03:19 +0530

    working on level

--- figmacopy/src/component/HeaderComponent.js
+++ figmacopy/src/component/HeaderComponent.js
@@ -9,14 +9,15 @@ function Header() {
             </div>
             <div className='nav__item nav__INR'>
                 <h4 className='front'>2500</h4>
-                <h4 className='later'>INR($)</h4>
+                <h4 className='later'>INR(&#x20b9;)</h4>
             </div>
             <div className='nav__item nav__notification'>
                 <div className='nav__notification--bell'></div>
                 <div className='nav__notification--red'></div>                            
             </div>
             <div className='nav__item nav__profile'>
-
+                <h4>Nishta</h4>
+                <div className='image'></div>
             </div>
         </div>
     );
--- figmacopy/src/component/LevelBodyComponents.js
+++ figmacopy/src/component/LevelBodyComponents.js
@@ -3,7 +3,38 @@ import React from 'react';
 function LevelBody(){ 
     return(
         <div className='levelbody'>
-            
+            <h3 className='top__heading'>Welcome Back, Nishta!</h3>
+            <div className=' mainbody'>
+                <div className='col__left'>
+                    <div className='col__left--box1'>
+                        <div className='box1__left  col__primary'> 
+                            <div className='papercircle papercircle--rd '>
+                                <div className='rdpaper'></div>
+                            </div>
+                            <div className='box1__left--text'> 
+                                <h3>See What's on pirority?</h3>
+                                <h4>Complete <mark>12 rejected questions</mark> to earn <mark>&#x20b9; 30/q</mark><mark></mark>&emsp;&emsp;<mark>&#8594;</mark></h4>
+                            </div>
+                        </div>
+                        <div className='box1__right col__primary'>
+                            <div className='papercircle papercircle--gr '>
+                                    <div className='grpaper'></div>
+                            </div>
+                            <div className='box1__right--text'> 
+                                <h3>New Task</h3>
+                                <h4>12&emsp;&emsp;<strong>&#8594;</strong></h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col__left--box2 col__primary'>
+
+                    </div>
+                    <div className='col__left--box3 col__primary'>
+
+                    </div>
+                </div>
+                <div className='col__right col__primary'></div>
+            </div>
         </div>
     );
 }
