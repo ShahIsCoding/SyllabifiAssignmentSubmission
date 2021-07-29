789b6bee67c666d3a17a3dcee7fec235613f3176
abhishekshah <56739982+abhishekshah0036@users.noreply.github.com>
2021-07-29 10:45:28 +0530

    on login

--- figmacopy/src/component/LoginComponent.js
+++ figmacopy/src/component/LoginComponent.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function LogIn() {
+class LogIn extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+                name:'',
+                email:'',
+                password:'',
+                repassword:''
+        }
+        this.value = this.value.bind(this);
+    }
+
+    value(e){
+        console.log(e.target.name,'  ',e.target.value);
+        this.setState({
+            [e.target.name]:e.target.value
+        });
+        console.log(this.state);
+    }
+    render(){
     return( 
         <div className='bg'>
             <div className='LogIn main-card'>
-                <img src={`${process.env.PUBLIC_URL}/assets/images/mobile.jpg`}  className='LogIn__img'/>      
+                <img src={`${process.env.PUBLIC_URL}/assets/images/mobile.jpg`}  className='LogIn__img' alt='mobile.jpg'/>      
                 <div className='LogIn__form'>
                     <div className='logo'>
                         <div className='logo__semicircle'></div>
@@ -18,39 +37,38 @@ function LogIn() {
                     <form className='Form'>
                         <div className='Form__divs'>
                             <label className='Form__divs--label'>Name</label>
-                            <input className='Form__divs--input' type='text' name='name' placeholder='Enter full name'/>
+                            <input className='Form__divs--input' type='text' name='name' placeholder='Enter full name' onBlur={this.value}/>
                         </div>
                         <div className='Form__divs'>
                             <label className='Form__divs--label'>Email</label>
-                            <input className='Form__divs--input' type='email' name='email' placeholder='Enter Email'/>
+                            <input className='Form__divs--input' type='email' name='email' placeholder='Enter Email' onBlur={this.value}/>
                         </div>
                         <div className='Form__divs'>
                             <label className='Form__divs--label'>Create Password</label>
-                           <input className='Form__divs--input' type='password' name='password' placeholder='Enter Password'/>
+                           <input className='Form__divs--input' type='password' name='password' placeholder='Enter Password' onBlur={this.value}/>
                         </div>
                         <div className='Form__divs'>
                             <label className='Form__divs--label'>Re-enter Password</label>
                            <input className='Form__divs--input' type='password' name='password' placeholder='Re-enter Password'/>
                         </div> 
-
-                    <div className='btns'>
-                        <div className='btns__b1'>
-                            <button className='btns__purple '>Sign Up &#8250;</button>
-                            <div className='login'>
-                                <h4> Already have an account?</h4>
-                                <a href='/desktop1'>Login here</a>
+                        <div className='btns'>
+                            <div className='btns__b1'>
+                                <a className='btn__signin btn_purple' href='/desktop1'>Sign Up &#8250;</a>
+                                <div className='login'>
+                                    <h4> Already have an account?</h4>
+                                    <a href='/desktop1'>Login here</a>
+                                </div>
                             </div>
-                        </div>
-                        <div className='btns__b2'>
-                            <h4> Or Sign Up with  </h4>
-                            <a href='#'>Goolge</a>
-                        </div>
-                    </div> 
+                            <div className='btns__b2'>
+                                <h4> Or Sign Up with  </h4>
+                                <a href='#'>Goolge</a>
+                            </div>
+                        </div> 
                     </form>
                 </div>
             </div>
         </div>
-    );
+    );}
 }
 
 export default LogIn;
